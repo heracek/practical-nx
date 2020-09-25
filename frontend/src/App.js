18b53491dc54745b080edaa338f4fdc6a65295e8
Tomáš Horáček <tomas.horacek@gmail.com>
2020-09-25 14:34:30 +0200

    Initial version of frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { ScrollToTop } from 'src/atoms/';
+import { AuthProvider } from 'src/utils/auth';
+import { EnhancedApolloProvider } from 'src/utils/apollo';
+import { Routes } from 'src/Routes';
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <EnhancedApolloProvider>
+          <ScrollToTop />
+          <Routes />
+        </EnhancedApolloProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
