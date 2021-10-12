a612a76312c663ea6c13f0ef3b140599b3ce0386
Test Test01 <tomas.horacek+test01@gmail.com>
2021-10-12 15:07:49 +0200

    Add solution for 1st Homework

--- frontend/src/pages/Practical01.js
+++ frontend/src/pages/Practical01.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Button, Heading } from 'src/atoms';
 
 export function Practical01() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div>
       <Heading>Practical 01</Heading>
-      <p>Hello!</p>
-      <Button onClick={() => alert('Button Pressed!')}>Press Me</Button>
+      <p>Counter: {counter}</p>
+      <Button onClick={() => setCounter(counter + 1)}>+1</Button>
+      <Button onClick={() => setCounter(0)}>Reset</Button>
+      <Button onClick={() => setCounter(counter - 1)}>-1</Button>
     </div>
   );
 }
